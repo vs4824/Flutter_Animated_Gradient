2252c1d5d58892ea4da7fcf646e01f27cabcee4a
Vaibhav Sharma <vaibhav.2022@tecorb.co>
2023-05-17 15:06:34 +0530

    Flutter Animated Gradient

---
+++ android/app/src/main/kotlin/com/example/flutter_animated_gradient/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_animated_gradient
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:animate_gradient/animate_gradient.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Animated Gradient',
+      home: App(),
+    );
+  }
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AnimateGradient(
+        primaryBegin: Alignment.topLeft,
+        primaryEnd: Alignment.bottomLeft,
+        secondaryBegin: Alignment.bottomLeft,
+        secondaryEnd: Alignment.topRight,
+        primaryColors: const [
+          Colors.pink,
+          Colors.pinkAccent,
+          Colors.white,
+        ],
+        secondaryColors: const [
+          Colors.white,
+          Colors.blueAccent,
+          Colors.blue,
+        ],
+        child: const Center(
+          child: Text(
+            'Animated Gradient',
+            style: TextStyle(
+              fontSize: 36,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
